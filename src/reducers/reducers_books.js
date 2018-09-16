83f4790e9ce2b531cfa8a4759dccaf19dd85cd05
LuisCasas <sc.luis@gmail.com>
2018-09-16 23:08:33 +0100

    Update lodash version

---
+++ src/reducers/reducers_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Java Masterclass'},
+        {title: 'C++ '},
+        {title: 'JavaScript'},
+        {title: 'IT For Dummies'}
+    ];
+}
